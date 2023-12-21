f4ebeadec7e6f2db5aa44041f8ea3d0b81520bc5
Vignesh.N <Vignesh.N@Saint-gobain.com>
2023-12-21 12:39:37 +0530

    changes 1

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace sampleDocker.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Vicky"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
